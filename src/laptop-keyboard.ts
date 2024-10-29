6882a5f9bbee95ba9b7057595475c911a4542c0d
Carlos Florêncio <carlosmflorencio@gmail.com>
2024-10-29 08:38:26 +0000

    improve keyboard bindings

--- src/laptop-keyboard.ts
+++ src/laptop-keyboard.ts
@@ -1,38 +1,42 @@
-
-import { FromKeyParam, map, rule, ToKeyParam, withMapper, withModifier } from 'karabiner.ts'
-import { ifAppleKeyboard } from './patterns'
+import {
+  FromKeyParam,
+  map,
+  rule,
+  ToKeyParam,
+  withMapper,
+  withModifier,
+} from "karabiner.ts";
+import { ifAppleKeyboard } from "./patterns";
 
 export function laptop_keyboard() {
-    return rule('Laptop Keyboard Bindings', ifAppleKeyboard).manipulators([
-        // Caps Lock → Hyper 
-        map('caps_lock').toHyper().toIfAlone('escape').parameters({
-            "basic.to_if_alone_timeout_milliseconds": 200
-        }),
+  return rule("Laptop Keyboard Bindings", ifAppleKeyboard).manipulators([
+    // Caps Lock → Hyper
+    map("caps_lock").toHyper().toIfAlone("escape").parameters({
+      "basic.to_if_alone_timeout_milliseconds": 200,
+    }),
+    map("caps_lock", ["right_shift"]).to("escape", "right_shift"),
 
-        // Right cmd to control
-        map('right_command').to('left_control'),
+    // Right cmd to control
+    map("right_command").to("left_control"),
 
-        // Hyper + hjkl → Arrow keys
-        withMapper<FromKeyParam, ToKeyParam>({
-            j: 'down_arrow',
-            l: 'right_arrow',
-            k: 'up_arrow',
-            h: 'left_arrow',
-        })((k, v) => withModifier('Hyper')([
-            map(k).to(v),
-        ])
-        ),
+    // Hyper + hjkl → Arrow keys
+    withMapper<FromKeyParam, ToKeyParam>({
+      j: "down_arrow",
+      l: "right_arrow",
+      k: "up_arrow",
+      h: "left_arrow",
+    })((k, v) => withModifier("Hyper")([map(k).to(v)])),
 
-        // Hyper + s/f to switch spaces
-        withModifier('Hyper')([
-            map('f').to('right_arrow', ['left_control']),
-            map('s').to('left_arrow', ['left_control']),
-        ]),
+    // Hyper + s/f to switch spaces
+    withModifier("Hyper")([
+      map("f").to("right_arrow", ["left_control"]),
+      map("s").to("left_arrow", ["left_control"]),
+    ]),
 
-        // Hyper + r/w to switch tabs
-        withModifier('Hyper')([
-            map('r').to('right_arrow', ['command', 'option']),
-            map('w').to('left_arrow', ['command', 'option']),
-        ])
-    ])
+    // Hyper + r/w to switch tabs
+    withModifier("Hyper")([
+      map("r").to("right_arrow", ["command", "option"]),
+      map("w").to("left_arrow", ["command", "option"]),
+    ]),
+  ]);
 }
