00033e6f7cf4905eb506be67c2c964b0d36e0f54
Carlos Florêncio <carlosmflorencio@gmail.com>
2025-01-22 09:24:52 +0000

    updated

--- src/rules-apps.ts
+++ src/rules-apps.ts
@@ -1,124 +1,125 @@
 import {
-  ifApp,
-  map,
-  rule,
-  toKey,
-  withCondition,
-  withModifier,
+    ifApp,
+    map,
+    rule,
+    toKey,
+    withCondition,
+    withModifier,
 } from "karabiner.ts";
 import {
-  ifBrowser,
-  ifFinder,
-  ifFloatingTerminal,
-  ifNotFloatingTerminal,
-  regex,
+    ifBrowser,
+    ifFinder,
+    ifFloatingTerminal,
+    ifNotFloatingTerminal,
+    regex,
 } from "./patterns";
 import {
-  betterDeleteWord,
-  historyNavi,
-  openNvimGitRepo,
-  tapModifiers,
+    betterDeleteWord,
+    historyNavi,
+    openNvimGitRepo,
+    tapModifiers,
 } from "./utils";
 
 export function app_finder() {
-  return rule("Finder").manipulators([
-    // open finder from other apps
-    withCondition(ifBrowser)([
-      map("f", ["command", "shift"]).to$("open ~/Downloads"),
-    ]),
-    withCondition(ifFinder)([
-      map("f", ["command", "shift"]).to("l", ["option", "command"]),
-    ]),
-  ]);
+    return rule("Finder").manipulators([
+        // open finder from other apps
+        withCondition(ifBrowser)([
+            map("f", ["command", "shift"]).to$("open ~/Downloads"),
+        ]),
+        withCondition(ifFinder)([
+            map("f", ["command", "shift"]).to("l", ["option", "command"]),
+        ]),
+    ]);
 }
 
 export function app_outlook() {
-  return rule("Outlook", ifApp(regex.outlook)).manipulators([
-    ...betterDeleteWord(),
+    return rule("Outlook", ifApp(regex.outlook)).manipulators([
+        ...betterDeleteWord(),
 
-    map("d", ["control"]).to("delete_or_backspace", "command"), // delete
-    map("a", ["control"]).to("e", "control"), // archive
-    map("u", ["control"]).to("f", ["command", "shift"]), // mark as unread
+        map("d", ["control"]).to("delete_or_backspace", "command"), // delete
+        map("a", ["control"]).to("e", "control"), // archive
+        map("u", ["control"]).to("f", ["command", "shift"]), // mark as unread
 
-    map("right_arrow", ["command", "option"]).to("]", ["shift", "control"]),
-    map("left_arrow", ["command", "option"]).to("[", ["shift", "control"]),
-  ]);
+        map("right_arrow", ["command", "option"]).to("]", ["shift", "control"]),
+        map("left_arrow", ["command", "option"]).to("[", ["shift", "control"]),
+    ]);
 }
 
 export function app_msty() {
-  return rule("Msty AI", ifApp(regex.msty)).manipulators([
-    ...betterDeleteWord(),
+    return rule("Msty AI", ifApp(regex.msty)).manipulators([
+        ...betterDeleteWord(),
 
-    map("t", ["command"]).to("s", ["command", "shift"]), // toggle context shield
-    map("n", ["command", "shift"]).to("s", ["command", "shift"]), // toggle context shield
-    map("k", ["command"]).to("f", ["command"]), // toggle search
-    map("b", ["command"]).to("1", ["command"]), // toggle side bar
+        map("t", ["command"]).to("s", ["command", "shift"]), // toggle context shield
+        map("r", ["command"]).to("s", ["command", "shift"]), // toggle context shield
+        map("n", ["command", "shift"]).to("s", ["command", "shift"]), // toggle context shield
+        map("k", ["command"]).to("f", ["command"]), // toggle search
+        map("b", ["command"]).to("1", ["command"]), // toggle side bar
 
-    map("right_arrow", ["command", "option"]).to("right_arrow", ["option"]),
-    map("left_arrow", ["command", "option"]).to("left_arrow", ["option"]),
-  ]);
+        map("right_arrow", ["command", "option"]).to("right_arrow", ["option"]),
+        map("left_arrow", ["command", "option"]).to("left_arrow", ["option"]),
+    ]);
 }
 
 export function app_chrome() {
-  return rule("Chrome", ifApp(regex.chrome)).manipulators([
-    ...betterDeleteWord(),
+    return rule("Chrome", ifApp(regex.chrome)).manipulators([
+        ...betterDeleteWord(),
 
-    map("o", ["control"]).to$(openNvimGitRepo()),
+        map("o", ["control"]).to$(openNvimGitRepo()),
 
-    ...tapModifiers({
-      "‹⌥": toKey("a", "⌘⇧"), // searchTabs
-      //'<⌘': toKey('i', '⌘⌥'), // developerTools
-    }),
+        ...tapModifiers({
+            "‹⌥": toKey("a", "⌘⇧"), // searchTabs
+            //'<⌘': toKey('i', '⌘⌥'), // developerTools
+        }),
 
-    map("i", ["command"]).to("i", ["command", "option"]), // developerTools
-  ]);
+        map("i", ["command"]).to("i", ["command", "option"]), // developerTools
+    ]);
 }
 
 export function app_slack() {
-  return rule("Slack", ifApp(regex.slack)).manipulators([
-    ...historyNavi(),
+    return rule("Slack", ifApp(regex.slack)).manipulators([
+        ...historyNavi(),
 
-    ...tapModifiers({
-      "<⌥": toKey("d", "⌘⇧"), // showHideSideBar
-      "<⌘": toKey(".", "⌘"), // hideRightBar
-    }),
-  ]);
+        ...tapModifiers({
+            "<⌥": toKey("d", "⌘⇧"), // showHideSideBar
+            "<⌘": toKey(".", "⌘"), // hideRightBar
+        }),
+    ]);
 }
 
 export function app_chatgpt() {
-  return rule("ChatGPT", ifApp(regex.chatgpt)).manipulators([
-    ...betterDeleteWord(),
+    return rule("ChatGPT", ifApp(regex.chatgpt)).manipulators([
+        ...betterDeleteWord(),
 
-    map("b", "command").to("f6"), // showHideSideBar
+        map("b", "command").to("f6"), // showHideSideBar
 
-    ...tapModifiers({
-      "<⌥": toKey("f6"), // showHideSideBar
-    }),
-  ]);
+        ...tapModifiers({
+            "<⌥": toKey("f6"), // showHideSideBar
+        }),
+    ]);
 }
 
 export function app_vivaldi() {
-  return rule("Vivaldi", ifApp(regex.vivaldi)).manipulators([
-    ...betterDeleteWord(),
+    return rule("Vivaldi", ifApp(regex.vivaldi)).manipulators([
+        ...betterDeleteWord(),
 
-    map("o", ["control"]).to$(openNvimGitRepo()),
+        map("o", ["control"]).to$(openNvimGitRepo()),
 
-    ...tapModifiers({
-      "‹⌥": toKey("e", "⌘"), // search tabs
-      //'<⌘': toKey('i', '⌘⌥'), // developerTools
-    }),
+        ...tapModifiers({
+            "‹⌥": toKey("e", "⌘"), // search tabs
+            //'<⌘': toKey('i', '⌘⌥'), // developerTools
+        }),
 
-    map("i", ["command"]).to("i", ["command", "option"]), // developerTools
-  ]);
+        map("i", ["command"]).to("i", ["command", "option"]), // developerTools
+    ]);
 }
 
 export function floating_terminal() {
-  return rule("Floating terminal").manipulators([
-    withCondition(ifNotFloatingTerminal)([
-      withModifier("Hyper")([map("return_or_enter").toApp("WezTerm")]),
-    ]),
-    withCondition(ifFloatingTerminal)([
-      withModifier("Hyper")([map("return_or_enter").to("tab", ["command"])]),
-    ]),
-  ]);
+    return rule("Floating terminal").manipulators([
+        withCondition(ifNotFloatingTerminal)([
+            withModifier("Hyper")([map("return_or_enter").toApp("WezTerm")]),
+        ]),
+        withCondition(ifFloatingTerminal)([
+            withModifier("Hyper")([map("return_or_enter").to("tab", ["command"])]),
+        ]),
+    ]);
 }
