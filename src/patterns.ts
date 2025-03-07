c12a266f2a8af29d98ad6ca6cc29deea0734570a
Carlos Florêncio <carlosmflorencio@gmail.com>
2025-03-07 16:53:48 +0000

    Updated files 2025-03-07 16:53:48

--- src/patterns.ts
+++ src/patterns.ts
@@ -2,22 +2,23 @@ import { ifApp, ifDevice } from "karabiner.ts";
 
 // osascript -e 'id of app "Application Name"'
 export const regex = {
-    chrome: "^com\\.google\\.Chrome$",
-    safari: "^com\\.apple\\.Safari$",
-    firefox: "^org\\.mozilla\\.firefox$",
-    vivaldi: "^com\\.vivaldi\\.Vivaldi$",
-    finder: "^com\\.apple\\.finder$",
-    iterm2: "^com\\.googlecode\\.iterm2$",
-    alacritty: "^org\\.alacritty$",
-    wezterm: "^com\\.github\\.wez\\.wezterm$",
-    activityMonitor: "^com\\.apple\\.ActivityMonitor$",
-    outlook: "^com\\.microsoft\\.Outlook$",
-    slack: "^com\\.tinyspeck\\.slackmacgap$",
-    chatgpt: "^com\\.openai\\.chat$",
-    msty: "^app\\.msty\\.app$",
-    chatbox: "^xyz\\.chatboxapp\\.app$",
-    browsers: [""],
-    terminals: [""],
+  chrome: "^com\\.google\\.Chrome$",
+  safari: "^com\\.apple\\.Safari$",
+  firefox: "^org\\.mozilla\\.firefox$",
+  vivaldi: "^com\\.vivaldi\\.Vivaldi$",
+  finder: "^com\\.apple\\.finder$",
+  iterm2: "^com\\.googlecode\\.iterm2$",
+  alacritty: "^org\\.alacritty$",
+  wezterm: "^com\\.github\\.wez\\.wezterm$",
+  warp: "^dev\\.warp\\.Warp-Stable$",
+  activityMonitor: "^com\\.apple\\.ActivityMonitor$",
+  outlook: "^com\\.microsoft\\.Outlook$",
+  slack: "^com\\.tinyspeck\\.slackmacgap$",
+  chatgpt: "^com\\.openai\\.chat$",
+  msty: "^app\\.msty\\.app$",
+  chatbox: "^xyz\\.chatboxapp\\.app$",
+  browsers: [""],
+  terminals: [""],
 };
 
 regex.browsers = [regex.chrome, regex.safari, regex.firefox, regex.vivaldi];
@@ -30,5 +31,15 @@ export const ifTerminal = ifApp(regex.terminals);
 export const ifFinder = ifApp(regex.finder);
 export const ifBrowser = ifApp(regex.browsers);
 
-export const ifFloatingTerminal = ifApp(regex.wezterm);
+export const floatingTerminal = {
+  app: "Wezterm",
+  regex: regex.wezterm,
+}
+
+// export const floatingTerminal = {
+//     app: "Warp",
+//     regex: regex.warp
+// }
+
+export const ifFloatingTerminal = ifApp(floatingTerminal.regex);
 export const ifNotFloatingTerminal = ifFloatingTerminal.unless();
